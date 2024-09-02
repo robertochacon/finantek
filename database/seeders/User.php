2d1c09b18578e017721818ebb4cf950f68ddf4ac
rchaconalcantara <hola.hack09@gmail.com>
2024-09-02 13:03:48 -0400

    Setup

---
+++ database/seeders/User.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User as ModelsUser;
+
+class User extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+
+        ModelsUser::create([
+            'name' => 'Administrador',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('admin'),
+            'remember_token' => null,
+            'created_at' => date("Y-m-d H:i:s")
+        ]);
+
+    }
+}
