194cc3cfabffee0e5741a96c8c6a295c5a06b686
rchaconalcantara <hola.hack09@gmail.com>
2024-09-16 13:16:25 -0400

    Avances en modulo de empleados

--- app/Filament/Resources/EmployeesResource/Pages/ListEmployees.php
+++ app/Filament/Resources/EmployeesResource/Pages/ListEmployees.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\EmployeesResource\Pages;
 use App\Filament\Resources\EmployeesResource;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
+use Illuminate\Database\Eloquent\Builder;
 
 class ListEmployees extends ListRecords
 {
@@ -15,5 +16,11 @@ class ListEmployees extends ListRecords
         return [
             Actions\CreateAction::make(),
         ];
+    }
+
+    protected function getTableQuery(): Builder
+    {
+        return parent::getTableQuery()
+            ->where('company_id', auth()->user()->company_id);
     }
 }
